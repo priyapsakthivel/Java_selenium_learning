ba294130b8f05a85d66607cdfc823ce568970ae5
priyapsakthivel <priyapsakthivel@gmail.com>
2021-07-17 21:51:39 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -7,5 +7,6 @@ public class TestNgMavenExample {
 	@Test
 	public void exampleOfTestNgMaven() {
 		System.out.println("This is TestNG-Maven Examples");
+		System.out.println("This is TestNG-Maven Example1 ");
 	}
 }
