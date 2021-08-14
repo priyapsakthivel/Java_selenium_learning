0286b7247ce222c4535f26a71680d61c4957d36b
priyaps <priyapsakthivel@gmail.com>
2021-08-14 23:02:50 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/FindIfStringHasVowels.java
+++ src/test/java/com/javacodegeeks/testng/maven/FindIfStringHasVowels.java
@@ -2,6 +2,12 @@ package com.javacodegeeks.testng.maven;
 
 public class FindIfStringHasVowels {
     public static void main(String[] args) {
-
+        String voweltype="qwertyuiopoiuytrsasdfghjklmnbvcxz";
+        int count=0;
+        for (int a = 0; a <voweltype.length() ; a++) {
+            if (voweltype.contains("a")){
+                count++;
+            }
+        }System.out.println(count);
     }
 }
