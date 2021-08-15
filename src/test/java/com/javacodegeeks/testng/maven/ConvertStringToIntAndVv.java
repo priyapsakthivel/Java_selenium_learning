86a60af6c27e953ba5b5ece8dc48f38a861d6e16
priya <priyapsakthivel@gmail.com>
2021-08-15 22:06:30 +0530

    Hello Commit

---
+++ src/test/java/com/javacodegeeks/testng/maven/ConvertStringToIntAndVv.java
@@ -0,0 +1,9 @@
+package com.javacodegeeks.testng.maven;
+
+public class ConvertStringToIntAndVv {
+    public static void main(String[] args) {
+        String name="12345";
+        int num=987654;
+
+    }
+}
