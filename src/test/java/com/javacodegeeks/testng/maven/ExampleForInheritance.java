aadc7e6eb7ea3bcb622f609f8c43522dc434e5e5
priya <priyapsakthivel@gmail.com>
2021-08-16 21:55:11 +0530

    Hello Commit

---
+++ src/test/java/com/javacodegeeks/testng/maven/ExampleForInheritance.java
@@ -0,0 +1,15 @@
+package com.javacodegeeks.testng.maven;
+
+public class ExampleForInheritance {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        ExampleForInheritance example= new ExampleForInheritance();
+        example.add(a,b);
+    }
+
+    public void add(10,12) {
+        System.out.println(a+b);
+
+    }
+}
