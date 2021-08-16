d0ee9988b1479f1f6afc3de184357a99fa88a5c6
priya <priyapsakthivel@gmail.com>
2021-08-16 21:13:27 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/ConvertStringToIntAndVv.java
+++ src/test/java/com/javacodegeeks/testng/maven/ConvertStringToIntAndVv.java
@@ -2,8 +2,16 @@ package com.javacodegeeks.testng.maven;
 
 public class ConvertStringToIntAndVv {
     public static void main(String[] args) {
-        String name="12345";
-        int num=987654;
+        String name = "1234";
+        int numbers=1234567890;
+
+        //covert int to string
+        String numchanged=Integer.toString(numbers);
+        System.out.println(numchanged);
+
+        //covert string to int
+        int namechanged=Integer.parseInt(name);
+        System.out.println(namechanged);
 
     }
 }
---
+++ src/test/java/com/javacodegeeks/testng/maven/MultiplesOf20.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.testng.maven;
+
+public class MultiplesOf20 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=10 ; i++) {
+            int multiples=i*20;
+            System.out.println(i+ "x 20 ="+multiples);
+        }
+    }
+}
