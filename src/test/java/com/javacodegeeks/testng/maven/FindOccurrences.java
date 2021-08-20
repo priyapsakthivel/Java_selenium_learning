c62800f55e174b0c50c35b73057d47d1a7a79873
priya <priyapsakthivel@gmail.com>
2021-08-20 22:04:54 +0530

    Hello Commit

---
+++ src/test/java/com/javacodegeeks/testng/maven/FindOccurrences.java
@@ -0,0 +1,29 @@
+package com.javacodegeeks.testng.maven;
+
+//String a ="iammohanraj";
+//find the occurrences of each word and its count
+
+import java.util.HashMap;
+
+public class FindOccurrences {
+
+    public static void main(String[] args) {
+        String a= "Mjdabdaaabkadgbkdkhds";
+        String[] strArr = a.split("");
+        HashMap<String,Integer> hashMap = new HashMap();
+
+
+        for (String s : strArr) {
+            if(hashMap.containsKey(s)){
+                int m = hashMap.get(s);
+                hashMap.put(s,m++);;
+            }else{
+                hashMap.put(s,1);
+            }
+        }
+
+        System.out.println(hashMap);
+
+
+    }
+}
