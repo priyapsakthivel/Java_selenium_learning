c292bd648b8e1dc6eb72f1a73e870f7388e68049
priya <priyapsakthivel@gmail.com>
2021-08-21 17:43:31 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/AugTwentyOne.java
+++ src/test/java/com/javacodegeeks/testng/maven/AugTwentyOne.java
@@ -2,15 +2,14 @@ package com.javacodegeeks.testng.maven;
 
 public class AugTwentyOne {
     public static void main(String[] args) {
-        int[] num={1234,1,2,3,4,1,5,52,54,987,12};
-        int count=num[0];
-
-        for(int i=0;i< num.length;i++){
-            if (num[i]<count){
-                count=num[i];
+    String name="qwertasaayuipkaaaajhgfdsaqwertaaaayuiopasdfghjkl";
+    String[]namearray=name.split("");
+    int count=0;
+        for (int i = 0; i < namearray.length; i++) {
+            if (namearray[i].contains("a")){
+                count++;
             }
-        }
-        System.out.println(count);
+        }System.out.println(count);
     }
 }
 
@@ -20,6 +19,16 @@ public class AugTwentyOne {
     int[] num={1234,1,2,3,4,1,5,52,54,987,12};
         for (int i=0;i<num.length;i++){
         if(num[i]>count){
+        count=num[i];
+        }
+        }
+        System.out.println(count);*/
+/*
+int[] num={1234,1,2,3,4,1,5,52,54,987,12};
+    int count=num[0];
+
+        for(int i=0;i< num.length;i++){
+        if (num[i]<count){
         count=num[i];
         }
         }
