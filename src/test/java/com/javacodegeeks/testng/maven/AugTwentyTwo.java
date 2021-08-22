92c8e6886bc311e00dfbec24dc97a00c4ad98bac
priya <priyapsakthivel@gmail.com>
2021-08-22 20:46:32 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/AugTwentyTwo.java
+++ src/test/java/com/javacodegeeks/testng/maven/AugTwentyTwo.java
@@ -2,20 +2,31 @@ package com.javacodegeeks.testng.maven;
 
 public class AugTwentyTwo {
     public static void main(String[] args) {
+    int num=12;
+    if (num%2==0){
+        System.out.println("its even");
+    }
+    else{
+        System.out.println("its odd");
+    }
+    }
+}
+
+
+
+/*
     int []num={1,1,2,2,333,1231,3,2};
     int first=0;
     int second=0;
         for (int i = 0; i < num.length; i++) {
-            if(num[i]>first){
-                second=first;
-                first=num[i];
-            }
-            else if (num[i]>second){
-                second=num[i];
+        if(num[i]>first){
+        second=first;
+        first=num[i];
+        }
+        else if (num[i]>second){
+        second=num[i];
 
-            }
+        }
         }
         System.out.println("first="+first);
-        System.out.println("second="+second);
-    }
-}
+        System.out.println("second="+second);*/
