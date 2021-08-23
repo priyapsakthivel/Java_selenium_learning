7b398a25c0f8ca099a8b33c787f2eeba3a5f2ec6
priya <priyapsakthivel@gmail.com>
2021-08-23 21:49:32 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/AugTwentyThree.java
+++ src/test/java/com/javacodegeeks/testng/maven/AugTwentyThree.java
@@ -2,6 +2,12 @@ package com.javacodegeeks.testng.maven;
 
 public class AugTwentyThree {
     public static void main(String[] args) {
-
+        int a=12;
+        int b=21;
+    AugTwentyThree augTwentyThree= new AugTwentyThree();
+    augTwentyThree.add(a,b);
+    }
+    public int add(12,21){
+        return(a+b)
     }
 }
