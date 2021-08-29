30f23d62f5b673163f5444c5139dd787ae265dae
priya <priyapsakthivel@gmail.com>
2021-08-29 23:37:59 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/Fibinoci.java
+++ src/test/java/com/javacodegeeks/testng/maven/Fibinoci.java
@@ -1,4 +1,10 @@
 package com.javacodegeeks.testng.maven;
 
 public class Fibinoci {
+    public static void main(String[] args) {
+        int n=0;
+        for (int i = 0; i < 12; i++) {
+           int r = i(i+1);
+        }
+    }
 }
