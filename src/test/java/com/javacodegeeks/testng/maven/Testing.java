fee0bf9aece9344990e46744220698a496d14b18
priya <priyapsakthivel@gmail.com>
2021-09-02 18:37:25 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/Testing.java
+++ src/test/java/com/javacodegeeks/testng/maven/Testing.java
@@ -2,10 +2,11 @@ package com.javacodegeeks.testng.maven;
 
 public class Testing {
     public static void main(String[] args) {
-        int count=0;
-        int[]arr={1,22,33,11112,34432,1234,1321,12322};
+        int[]arr={2222,1,22,33,11112,34432,1234,1321,12322};
+        int count=arr[0];
+
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>count){
+            if(arr[i]<count){
                 count=arr[i];
             }
         }
