2d1afe84b813337ca485fab395031981f09296a4
priya <priyapsakthivel@gmail.com>
2021-09-02 21:20:56 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/Testing.java
+++ src/test/java/com/javacodegeeks/testng/maven/Testing.java
@@ -2,18 +2,18 @@ package com.javacodegeeks.testng.maven;
 
 public class Testing {
     public static void main(String[] args) {
-        int[] arr = {12, 12, 321, 123, 321, 123543233, 2123454, 322134, 3212343, 212343};
-        int count = 0;
-        int temp = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > count) {
-                temp = count;
-                count = arr[i];
-
-            } else if (arr[i] > temp) {
-                temp = arr[i];
+        int[] arr = {1, 12, 321, 123, 321, 123543233, 2123454, 322134, 3212343, 212343};
+        int count = arr[0];
+        int temp =arr[0];
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i]<count){
+                temp=count;
+                count=arr[i];
             }
-
-        }System.out.println("second largest :" + temp + "  &" + "largest number :" + count);
+            else if(arr[i]>temp && temp>count) {
+                temp=arr[i];
+            }
+        }
+        System.out.println("second largest :" + temp + "  &" + "largest number :" + count);
+        }
     }
-}
