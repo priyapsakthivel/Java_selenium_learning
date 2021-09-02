8fdcc581157e4b4e41dbd8831411c60547ed6235
priya <priyapsakthivel@gmail.com>
2021-09-02 21:59:12 +0530

    Hello Commit

--- src/test/java/com/javacodegeeks/testng/maven/Testing.java
+++ src/test/java/com/javacodegeeks/testng/maven/Testing.java
@@ -1,19 +1,22 @@
 package com.javacodegeeks.testng.maven;
 
+import java.util.Scanner;
+
 public class Testing {
     public static void main(String[] args) {
-        int[] arr = {1, 12, 321, 123, 321, 123543233, 2123454, 322134, 3212343, 212343};
-        int count = arr[0];
-        int temp =arr[0];
-        for (int i = 0; i < arr.length ; i++) {
-            if(arr[i]<count){
-                temp=count;
-                count=arr[i];
-            }
-            else if(arr[i]>temp && temp>count) {
-                temp=arr[i];
-            }
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            int num = scanner.nextInt();
+        Testing testing = new Testing();
+        testing.oddEven(num);
+    }}
+
+    public Integer oddEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + "is an even number");
+        } else {
+            System.out.println(num + "is an odd number");
         }
-        System.out.println("second largest :" + temp + "  &" + "largest number :" + count);
-        }
+        return 0;
     }
+}
---
+++ src/test/java/com/javacodegeeks/testng/maven/Testing1.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.testng.maven;
+
+public class Testing1 {
+    public static void main(String[] args) {
+        int num= 1234;
+        if(num%2==0){
+
+        }
+    }
+}
