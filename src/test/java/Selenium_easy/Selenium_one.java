9f570c7954ade159a3028e1a489cf3aac9decc4b
priya <priyapsakthivel@gmail.com>
2021-09-06 23:25:44 +0530

    Hello Commit

--- src/test/java/Selenium_easy/Selenium_one.java
+++ src/test/java/Selenium_easy/Selenium_one.java
@@ -1,15 +1,19 @@
 package Selenium_easy;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.File;
 
 public class Selenium_one {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         File file = new File("P:\\Webdrivers\\edgedriver.exe");
         System.setProperty("webdriver.edge.driver",file.getAbsolutePath());
         WebDriver driver= new EdgeDriver();
         driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        driver.findElement(By.id("at-cv-lightbox-close")).click();
     }
 }
