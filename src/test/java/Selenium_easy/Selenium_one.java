aba1349ba7789c26e3c9010e73dbc177aadcfa16
priyaps <priyapsakthivel@gmail.com>
2021-09-28 23:15:55 +0530

    Hello Commit

--- src/test/java/Selenium_easy/Selenium_one.java
+++ src/test/java/Selenium_easy/Selenium_one.java
@@ -27,7 +27,7 @@ public class Selenium_one {
         jse.executeScript("scrollTo(0,300)");
         driver.findElement(By.id("sum1")).sendKeys("12345");
         driver.findElement(By.id("sum2")).sendKeys("23456");
-        Thread.sleep(300);
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"gettotal\"]/button")).click();
         String totalSum=driver.findElement(By.id("displayvalue")).getText();
         System.out.println(totalSum);
